4df30debc0e28fa0383d8d52cc29d0e79a950989
iammiro <miroslav.kolomiets@gmail.com>
2017-07-22 18:07:40 +0300

    edit styles

--- src/block/general-scripts/main.js
+++ src/block/general-scripts/main.js
@@ -1,17 +1,25 @@
 $('.mobile_menu_icon').click(function () {
     $('.navigation').toggle();
-
+    $('.mobile_menu_icon').hide();
+    $('.mobile_menu_icon_close').show();
+});
+$('.mobile_menu_icon_close').click(function () {
+    $('.navigation').toggle();
+    $('.mobile_menu_icon').show();
+    $('.mobile_menu_icon_close').hide();
 });
 
 (function($) {
     var $window = $(window),
-        $menu = $('.navigation');
+        $menu = $('.navigation'),
+        $mobileMenuIconClose = $('.mobile_menu_icon_close');
 
     $window.resize(function resize() {
         if ($window.width() > 990) {
-            return $menu.addClass('display_inline_flex');
+            return $menu.addClass('display_inline_flex'),
+                $mobileMenuIconClose.addClass('display_none');
         }
-
         $menu.removeClass('display_inline_flex');
+        $mobileMenuIconClose.removeClass('display_none');
     }).trigger('resize');
 })(jQuery);
